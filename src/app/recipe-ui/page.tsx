96eecefd7a6d7fa5911148e49d45191ab4061d55
josueJURE <josue.jure@gmail.com>
2025-11-27 12:20:59 +0000

    (chore): delete unused code

--- src/app/recipe-ui/page.tsx
+++ src/app/recipe-ui/page.tsx
@@ -17,7 +17,7 @@ import {
 import { Button } from "@/components/ui/button";
 
 import DietaryRequirements from "@/components/ui/dietary-requirements";
-import { Input } from "@/components/ui/input";
+
 
 export default function Test() {
   const [selectedCountry, setSelectedCountry] = useState<string>("");
@@ -27,10 +27,6 @@ export default function Test() {
   const [otherDietaryRequirements, setOtherDietaryRequirements] =
     useState<boolean>(false);
 
-  // const handlleDietaryRequirements = () => {
-  //   setOtherDietaryRequirements((prev) => !prev);
-
-  // };
 
   const handleDietaryRequirements = (checked: boolean) => {
     setOtherDietaryRequirements(checked);
@@ -46,11 +42,10 @@ export default function Test() {
         <form className="w-full max-w-xl p-6 relative bg-gray-700 rounded-2xl">
           <Card className=" flex items-center">
             <DietaryRequirements onToggle={handleDietaryRequirements} />
-            {/* {otherDietaryRequirements && <Input type="text" className="w-0.25xl" />} */}
 
-            {selectedCountry && ( <div>{`You want a dish from ${selectedCountry}`}</div>) }
-           
-       
+            {selectedCountry && (
+              <div>{`You want a dish from ${selectedCountry}`}</div>
+            )}
 
             <Map
               handleCountrySelect={handleCountrySelect}
