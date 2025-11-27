89b83579b9257007ced2280248794033398bf298
josueJURE <josue.jure@gmail.com>
2025-11-27 12:25:08 +0000

    (chore): delete unused code

--- src/app/recipe-ui/page.tsx
+++ src/app/recipe-ui/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+
 // #Gladiator2000
 
 import Map from "@/components/map";
+import { Button } from "@/components/ui/button";
 
 import { useState } from "react";
 
@@ -14,10 +16,9 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-import { Button } from "@/components/ui/button";
 
 import DietaryRequirements from "@/components/ui/dietary-requirements";
-
+import { Input } from "@/components/ui/input";
 
 export default function Test() {
   const [selectedCountry, setSelectedCountry] = useState<string>("");
@@ -29,8 +30,9 @@ export default function Test() {
 
 
   const handleDietaryRequirements = (checked: boolean) => {
-    setOtherDietaryRequirements(checked);
-  };
+    setOtherDietaryRequirements(checked)
+  }
+  
 
   const handleCountrySelect = (countryName: string) => {
     setSelectedCountry(countryName);
@@ -42,17 +44,17 @@ export default function Test() {
         <form className="w-full max-w-xl p-6 relative bg-gray-700 rounded-2xl">
           <Card className=" flex items-center">
             <DietaryRequirements onToggle={handleDietaryRequirements} />
+            {otherDietaryRequirements && <Input type="text" className="w-0.25xl" />}
 
-            {selectedCountry && (
-              <div>{`You want a dish from ${selectedCountry}`}</div>
-            )}
+            <div>{selectedCountry}</div>
 
             <Map
               handleCountrySelect={handleCountrySelect}
               isDarkMode={isDarkMode}
             />
-            <Button>Submit</Button>
+               <Button>Submit</Button>
           </Card>
+       
         </form>
       </main>
     </>
